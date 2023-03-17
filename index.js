98e591c1fbf479a808d023676df51267f0f280c8
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-03-17 18:27:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+// beforeEach(function () {
+//     cats.length = 0;
+  
+//     cats.push("Milo", "Otis", "Garfield");
+//   });
+//   console.log()
+const cats=["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push('Ralph')
+}
+ function destructivelyPrependCat(){
+    cats.unshift("Bob")
+
+ }  
+ function destructivelyRemoveLastCat(){
+    cats.pop()
+ } 
+ function destructivelyRemoveFirstCat(){
+    cats.shift()
+ }
+ function appendCat(name){
+    const cat=[...cats]
+    cat.push("Broom")
+    return cat;
+
+ }
+ function prependCat(name){
+    const cat=[...cats];
+    cat.unshift("Arnold")
+    return cat
+
+ }
+ function removeLastCat(){
+    const cat=[...cats]
+    cat.pop("Garfield")
+    return cat;
+
+    
+
+ }
+function removeFirstCat(){
+    const cat=[...cats]
+    cat.shift("Milo")
+    return cat;
+}
